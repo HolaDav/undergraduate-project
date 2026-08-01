b348e332d3c1b3e2ad863c9a6a868d26cb7028e6
HolaDav <oladejidavid12@gmail.com>
2026-08-01 22:55:21 +0000

    Module 1 - Coregistration; Module 2 - Segmentation

---
+++ scripts/spm_preprocessing_job_template_job.m
@@ -0,0 +1,50 @@
+%-----------------------------------------------------------------------
+% Job saved on 01-Aug-2026 22:45:50 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - Unknown
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.coreg.estimate.ref = {'/home/jovyan/Desktop/workspace/undergraduate_project/rawdata/sub-AD01/anat/sub-AD01_T1w.nii,1'};
+matlabbatch{1}.spm.spatial.coreg.estimate.source = {'/home/jovyan/Desktop/workspace/undergraduate_project/rawdata/sub-AD01/pet/sub-AD01_trc-pib_pet.nii,1'};
+matlabbatch{1}.spm.spatial.coreg.estimate.other = {''};
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
+matlabbatch{2}.spm.spatial.preproc.channel.vols = {'/home/jovyan/Desktop/workspace/undergraduate_project/rawdata/sub-AD01/anat/sub-AD01_T1w.nii,1'};
+matlabbatch{2}.spm.spatial.preproc.channel.biasreg = 0.001;
+matlabbatch{2}.spm.spatial.preproc.channel.biasfwhm = 60;
+matlabbatch{2}.spm.spatial.preproc.channel.write = [1 1];
+matlabbatch{2}.spm.spatial.preproc.tissue(1).tpm = {'/opt/spm12/spm12_mcr/spm12/spm12/tpm/TPM.nii,1'};
+matlabbatch{2}.spm.spatial.preproc.tissue(1).ngaus = 1;
+matlabbatch{2}.spm.spatial.preproc.tissue(1).native = [1 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(1).warped = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(2).tpm = {'/opt/spm12/spm12_mcr/spm12/spm12/tpm/TPM.nii,2'};
+matlabbatch{2}.spm.spatial.preproc.tissue(2).ngaus = 1;
+matlabbatch{2}.spm.spatial.preproc.tissue(2).native = [1 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(2).warped = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(3).tpm = {'/opt/spm12/spm12_mcr/spm12/spm12/tpm/TPM.nii,3'};
+matlabbatch{2}.spm.spatial.preproc.tissue(3).ngaus = 2;
+matlabbatch{2}.spm.spatial.preproc.tissue(3).native = [1 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(3).warped = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(4).tpm = {'/opt/spm12/spm12_mcr/spm12/spm12/tpm/TPM.nii,4'};
+matlabbatch{2}.spm.spatial.preproc.tissue(4).ngaus = 3;
+matlabbatch{2}.spm.spatial.preproc.tissue(4).native = [1 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(4).warped = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(5).tpm = {'/opt/spm12/spm12_mcr/spm12/spm12/tpm/TPM.nii,5'};
+matlabbatch{2}.spm.spatial.preproc.tissue(5).ngaus = 4;
+matlabbatch{2}.spm.spatial.preproc.tissue(5).native = [1 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(5).warped = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(6).tpm = {'/opt/spm12/spm12_mcr/spm12/spm12/tpm/TPM.nii,6'};
+matlabbatch{2}.spm.spatial.preproc.tissue(6).ngaus = 2;
+matlabbatch{2}.spm.spatial.preproc.tissue(6).native = [0 0];
+matlabbatch{2}.spm.spatial.preproc.tissue(6).warped = [0 0];
+matlabbatch{2}.spm.spatial.preproc.warp.mrf = 1;
+matlabbatch{2}.spm.spatial.preproc.warp.cleanup = 1;
+matlabbatch{2}.spm.spatial.preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
+matlabbatch{2}.spm.spatial.preproc.warp.affreg = 'mni';
+matlabbatch{2}.spm.spatial.preproc.warp.fwhm = 0;
+matlabbatch{2}.spm.spatial.preproc.warp.samp = 3;
+matlabbatch{2}.spm.spatial.preproc.warp.write = [0 1];
+matlabbatch{2}.spm.spatial.preproc.warp.vox = NaN;
+matlabbatch{2}.spm.spatial.preproc.warp.bb = [NaN NaN NaN
+                                              NaN NaN NaN];
